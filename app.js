a4806f06ce3357c952fcbab6b5cd509b80d49221
sivakarthikeyanoptit <sivakarthikeyan.s@optit.co>
2021-05-03 11:38:18 +0530

    updating the docker file

--- app.js
+++ app.js
@@ -1,151 +1,22 @@
-require("dotenv").config();
-
-//express
-const express = require("express");
-let app = express();
-
-// Health check
-require("./healthCheck")(app);
-
-//config and routes
-global.config = require("./config");
-require("./config/globalVariable")();
-
-let environmentData = require("./envVariables")();
-
-if(!environmentData.success) {
-  log.warning("Server could not start . Not all environment variable is provided");
-  process.exit();
-}
-
-let router = require("./routes");
-
-//required modules
-const fileUpload = require("express-fileupload");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-var fs = require("fs");
-var path = require("path");
-var expressValidator = require('express-validator');
-
-//To enable cors
-app.use(cors());
-app.use(expressValidator())
-
-
-//health check
-app.get("/ping", (req, res) => {
-  res.send("pong!");
-});
-
-app.use(fileUpload());
-app.use(bodyParser.json({ limit: '50MB' }));
-app.use(bodyParser.urlencoded({ limit: '50MB', extended: false }));
-app.use(express.static("public"));
-
-fs.existsSync("logs") || fs.mkdirSync("logs");
-
-const serviceBaseUrl = process.env.APPLICATION_BASE_URL || "/assessment/";
-
-const observationSubmissionsHtmlPath = process.env.OBSERVATION_SUBMISSIONS_HTML_PATH ? process.env.OBSERVATION_SUBMISSIONS_HTML_PATH : "observationSubmissions"
-app.use(express.static(observationSubmissionsHtmlPath));
-app.get(serviceBaseUrl+observationSubmissionsHtmlPath+"/*", (req, res) => {
-  let urlArray = req.path.split("/")
-  urlArray.splice(0,3)
-  res.sendFile(path.join(__dirname, "/public/"+observationSubmissionsHtmlPath+"/"+urlArray.join("/")));
-});
-
-//API documentation (apidoc)
-if (process.env.NODE_ENV == "development" || process.env.NODE_ENV == "local") {
-  app.use(express.static("apidoc"));
-  if(process.env.NODE_ENV == "local") {
-    app.get("/apidoc", (req, res) => {
-      res.sendFile(path.join(__dirname, "/public/apidoc/index.html"));
-    });
-  } else {
-    app.get(serviceBaseUrl+"apidoc/*", (req, res) => {
-      let urlArray = req.path.split("/")
-      urlArray.splice(0,3)
-      res.sendFile(path.join(__dirname, "/public/apidoc/"+urlArray.join("/")));
-    });
-  }
-}
-
-  // app.get(serviceBaseUrl+"web/*", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "/public/assessment/web/index.html"));
-  // });
-
-app.get(serviceBaseUrl + "web2/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public" + serviceBaseUrl + "web2/index.html"));
-});
-
-var bunyan = require("bunyan");
-
-global.loggerObj = bunyan.createLogger({
-  name: "foo",
-  streams: [
-    {
-      type: "rotating-file",
-      path: path.join(__dirname + "/logs/" + process.pid + "-all.log"),
-      period: "1d", // daily rotation
-      count: 3 // keep 3 back copies
-    }
-  ]
-});
-
-global.loggerExceptionObj = bunyan.createLogger({
-  name: "exceptionLogs",
-  streams: [
-    {
-      type: "rotating-file",
-      path: path.join(__dirname + "/logs/" + process.pid + "-exception.log"),
-      period: "1d", // daily rotation
-      count: 3 // keep 3 back copies
-    }
-  ]
-});
-
-app.all("*", (req, res, next) => {
-  if(ENABLE_BUNYAN_LOGGING === "ON") {
-    loggerObj.info({
-      method: req.method,
-      url: req.url,
-      headers: req.headers,
-      body: req.body
-    });
-  }
-
-  if(ENABLE_DEBUG_LOGGING === "ON") {
-    log.info("-------Request log starts here------------------");
-    log.info(
-      "%s %s on %s from ",
-      req.method,
-      req.url,
-      new Date(),
-      req.headers["user-agent"]
-    );
-    log.info("Request Headers: ", req.headers);
-    log.info("Request Body: ", req.body);
-    log.info("Request Files: ", req.files);
-    log.info("-------Request log ends here------------------");
-  }
-  
-  next();
-});
-
-
-//add routing
-router(app);
-
-//listen to given port
-app.listen(config.port, () => {
-
-  log.info(
-    "Environment: " +
-    (process.env.NODE_ENV ? process.env.NODE_ENV : "development")
-  );
-
-  log.info("Application is running on the port:" + config.port);
-
-});
-
+console.log("-------Deployment testing starts here------------------");
+console.log(process.env.APPLICATION_PORT)
+console.log(process.env.APPLICATION_ENV)
+console.log(process.env.MONGODB_URL)
+console.log(process.env.USER_SERVICE_URL)
+console.log(process.env.INTERNAL_ACCESS_TOKEN)
+console.log(process.env.KAFKA_COMMUNICATIONS_ON_OFF)
+console.log(process.env.KAFKA_URL)
+console.log(process.env.SUBMISSION_RATING_QUEUE_TOPIC)
+console.log(process.env.COMPLETED_OBSERVATION_SUBMISSION_TOPIC)
+console.log(process.env.INCOMPLETE_OBSERVATION_SUBMISSION_TOPIC)
+console.log(process.env.COMPLETED_SURVEY_SUBMISSION_TOPIC)
+console.log(process.env.KAFKA_GROUP_ID)
+console.log(process.env.IMPROVEMENT_PROJECT_SUBMISSION_TOPIC)
+console.log(process.env.ELASTICSEARCH_COMMUNICATIONS_ON_OFF)
+console.log(process.env.ELASTICSEARCH_HOST_URL)
+console.log(process.env.ELASTIC_SEARCH_SNIFF_ON_START)
+console.log(process.env.ELASTICSEARCH_ENTITIES_INDEX)
+console.log(process.env.ML_CORE_SERVICE_URL)
+console.log(process.env.ML_PROJECT_SERVICE_URL)
+console.log(process.env.KEYCLOAK_PUBLIC_KEY_PATH)
+console.log("-------Deployment testing ends   here------------------");
--- build.sh
+++ build.sh
@@ -5,7 +5,5 @@ build_tag=$1
 name=ml-survey-service
 node=$2
 org=$3
-
-[[ -f ImageMagick-i386-pc-solaris2.11.tar.gz ]] || wget https://www.imagemagick.org/download/binaries/ImageMagick-i386-pc-solaris2.11.tar.gz
 docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
